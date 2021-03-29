2b1f90595585a2451ca76bf0f7e10169f8b7a67d
Harsh Pawar <pawarharsh21@gmail.com>
2021-03-29 11:00:07 +0530

    add validation for comfirm alert

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,33 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import validator from 'validator';
 
 import { UserContext } from '../../context/user-context/user-context';
 import './login.css';
 
-function Login() {
+function Login(props) {
 
-    const [emailId, setEmailId] = useState('');
-    const [password, setPassword] = useState('');
+    const [user, setUser] = useState({ emailId: '', password: '' });
     const userContext = useContext(UserContext);
 
     const onSubmitHandler = () => {
-        userContext.logInUser(emailId, password);
+        if (!validator.isEmail(user.emailId)) {
+            toast.error('Enter a valid email Id');
+        } else if (user.password.length < 6) {
+            toast.error('Password length should be atleast 6');
+        } else {
+            loginUser();
+        }
+    }
 
+    const loginUser = () => {
+        const { location: { state } } = props;
+        if (state && state.next) {
+            userContext.logInUser(user, state.next);
+        } else {
+            userContext.logInUser(user);
+        }
     }
     return (
         <div className="form-container">
@@ -23,13 +38,13 @@ function Login() {
                     Email Id
                 </div>
                 <div className="form__input">
-                    <input type="email" value={emailId} onChange={(e) => { setEmailId(e.currentTarget.value) }} />
+                    <input type="email" value={user.emailId} onChange={(e) => setUser({ ...user, emailId: e.target.value })} />
                 </div>
                 <div className="form__label">
                     Password
                 </div>
                 <div className="form__input">
-                    <input type="password" value={password} onChange={(e) => { setPassword(e.currentTarget.value) }} />
+                    <input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
                 </div>
                 <div className="forgot-password-link"><Link to="/forgot-password" className="link">Forgot password?</Link> </div>
                 <div className="form__submit-btn" onClick={onSubmitHandler}>Sign in</div>
